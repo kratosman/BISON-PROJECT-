3651642ca204207b78396204499fa43effa75a3b
Benjie silvano <103920174+kratosman@users.noreply.github.com>
2022-08-15 07:00:44 +0800

    Delete PROJECT directory

--- PROJECT/model/Comments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const CommentSchema = new mongoose.Schema({
-    postId: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
-    name:{ type: String, ref:'User'},
-    comment: { type:String, required: true },
-})
-
-module.exports = mongoose.model('Comments', CommentSchema)
--- PROJECT/model/UserMode.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const UserSchema = new mongoose.Schema({
-    // userId: { type: mongoose.Schema.Types.ObjectId, required: true},
-    username: { type:String, required: true },
-    password: { type:String, required: true}
-})
-
-module.exports = mongoose.model('User', UserSchema)
